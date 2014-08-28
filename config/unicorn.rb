3e0e8cd39ff72e8579dff91b8f25abae56c3868d
Robert McNeil (porty) <pppppppman@gmail.com>
2014-08-28 23:28:51 +1000

    Not logs, it's log

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -36,8 +36,8 @@ pid pid_path
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "#{rails_root}/logs/unicorn.stderr.log"
-stdout_path "#{rails_root}/logs/unicorn.stdout.log"
+stderr_path "#{rails_root}/log/unicorn.stderr.log"
+stdout_path "#{rails_root}/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
