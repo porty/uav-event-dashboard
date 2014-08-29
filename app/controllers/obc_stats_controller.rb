b524bd9c9447cc557a3d95aa8ec8ed1ce995ac53
Robert McNeil (porty) <pppppppman@gmail.com>
2014-08-30 00:29:40 +1000

    Simplified  obc stats view

--- app/controllers/obc_stats_controller.rb
+++ app/controllers/obc_stats_controller.rb
@@ -4,4 +4,15 @@ class ObcStatsController < ApplicationController
     @latest_backlog = Events::Backlog.last
   end
 
+  helper_method :timestamp_in_words
+  def timestamp_in_words(unix_timestamp)
+    time = Time.at(unix_timestamp)
+
+    prefix = view_context.time_ago_in_words(time, include_seconds: true)
+    suffix = time.future? ? 'in the future' : 'ago'
+
+    #view_context.time_ago_in_words(time) + time.future? ? 'in the future' : 'ago'
+    "#{prefix} #{suffix}"
+  end
+
 end
